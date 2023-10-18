2da9b2b8816c0a08c3d153229980af1bb82143f0
Ricardo <ric.dalvarez@hotmail.com>
2023-10-18 22:14:14 +0200

    add routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,6 +2,23 @@ import { Router } from "express";
 
 export const router = Router();
 
-router.get("/", (req, res) => {
-  res.send("Hello World!");
+router.get("/skins/available", (req, res) => {
+  res.send("5 skins available");
 });
+
+router.post("/skins/buy", (req, res) => {
+  res.send("You bought a skin");
+});
+
+router.get("/skins/myskins", (req, res) => {
+  res.send("You have 5 skins");
+});
+
+router.put("/skins/myskins/color", (req, res) => {
+  res.send("You changed the color of your skin");
+});
+
+router.delete("/skins/myskins/delete/${id}", (req, res) => {
+  res.send("You deleted a skin");
+});
+çç;
