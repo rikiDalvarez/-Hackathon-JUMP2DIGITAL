6b6ea025feb263b0dcec704bb1d8a5d29e71a6d9
Ricardo <ric.dalvarez@hotmail.com>
2023-10-19 19:41:28 +0200

    add routes

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,16 +3,15 @@ import ip from "ip";
 import cors from "cors";
 import "dotenv/config";
 import config from "../config/config";
+import { router } from "./routes";
 
 const app = exporess();
 
 const PORT = config.PORT || 3000;
+
 app.use(cors({ origin: "*" }));
 app.use(exporess.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/skins", router);
 
 app.listen(PORT, () => {
   console.log(`Server running at http://${ip.address()}:${PORT} 🍄`);
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,22 +2,23 @@ import { Router } from "express";
 
 export const router = Router();
 
-router.get("/skins/available", (req, res) => {
+router.get("/available", (req, res) => {
   res.send("5 skins available");
 });
 
-router.post("/skins/buy", (req, res) => {
+router.post("/buy", (req, res) => {
   res.send("You bought a skin");
 });
 
-router.get("/skins/myskins", (req, res) => {
+router.get("/myskins", (req, res) => {
   res.send("You have 5 skins");
 });
 
-router.put("/skins/myskins/color", (req, res) => {
+router.put("/myskins/color", (req, res) => {
   res.send("You changed the color of your skin");
 });
 
-router.delete("/skins/myskins/delete/${id}", (req, res) => {
+router.delete("/myskins/delete/:id", (req, res) => {
+  console.log(req.params.id);
   res.send("You deleted a skin");
 });
