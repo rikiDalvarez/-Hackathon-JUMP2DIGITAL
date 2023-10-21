9863a3bc86ff6bdf69701f5dbb0adebf0894e30f
Ricardo <ric.dalvarez@hotmail.com>
2023-10-21 19:55:59 +0200

    add querys

---
+++ server/src/query/skins.quer.ts
@@ -0,0 +1,9 @@
+const QUERY = {
+  SELECT_AVAILABLE_SKINS:
+    "SELECT * FROM skins WHERE quantity > 0 ORDER BY created_at DESC LIMIT 100",
+  SELECT_SKIN: "SELECT * FROM skins WHERE id = ?",
+  CREATE_SKIN:
+    "INSERT INTO skins(name, price, type, color, quantity) VALUES(?, ?, ?, ?, ?) ",
+  UPDATE_SKIN:
+    "UPDATE skins SET name = ?, price = ?, type = ?, color = ?, quantity = ? WHERE id = ?",
+};
