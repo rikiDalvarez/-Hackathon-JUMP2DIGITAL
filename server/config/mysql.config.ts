4ad9bec5bb97fa11ec6c189ee9629719a9debeef
Ricardo <ric.dalvarez@hotmail.com>
2023-10-21 19:29:39 +0200

    add quantity on skins

--- server/config/mysql.config.ts
+++ server/config/mysql.config.ts
@@ -4,7 +4,11 @@ import config from "./config";
 
 const pool = mysql.createPool({
   host: config.HOST,
+  port: config.PORT,
   user: config.MYSQL_USER,
   password: config.MYSQL_PASSWORD,
   database: config.DATABASE,
+  connectionLimit: 10,
 });
+
+export default pool;
